ee1366e0217a2643647382cdacbbb0da31b66cf4
Ranjitha <ranjitha.v.cse27@princedrkvasudevan.com>
2026-05-03 18:47:34 +0530

    Updated app.py and added hospital PDF for RAG system

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from backend import load_and_split, create_vector_store, ask_question
+
 print("Hospital Policy Assistant Started")
+
+texts = load_and_split()
+db = create_vector_store(texts)
+
+while True:
+    query = input("\nAsk a question (type 'exit' to quit): ")
+
+    if query.lower() == "exit":
+        break
+
+    answer = ask_question(db, query)
+    print("\nAnswer:", answer)
+    print("-" * 40)
